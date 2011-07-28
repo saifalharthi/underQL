2ee3783f329e4761e73e81bab717121b1200b04e
unknown <Abdullah Almehmadi@.(none)>
2011-07-28 04:23:27 +0300

    First version of underQL that is contains the core.

---
+++ underQL.php
@@ -0,0 +1,387 @@
+
+<?php
+
+ /*
+    UnderQL Project
+    By Abdullah E. Almehmadi
+     <cs.abdullah@hotmail.com>
+   6:25 am 26-08-32 : 2011-07-27
+   domain registered 6:32 am <www.underql.com>
+   1.0.0.Beta
+
+   */
+
+/* database host */
+$UNDERQL['db']['host']     = 'localhost';
+/* database name */
+$UNDERQL['db']['name']     = 'adeeb';
+/* database user name */
+$UNDERQL['db']['user']     = 'root';
+/* database password */
+$UNDERQL['db']['password'] = '';
+/* database encoding system for database operations */
+$UNDERQL['db']['encoding'] = 'utf8';
+
+/*
+  Here you should put all tables names that you inted to work with
+    like the following :
+
+  $UNDERQL['table']['your_table_name'] = array('field1','field2',...);
+
+  Each table has its own array. However, the array values are the names of
+   the STRING FIELDS that are accepting single qoutes in that table.
+
+   Note : Don't put any numerical field name in array.
+*/
+$UNDERQL['table']['adeeb_user'] = array('name','email','password','description','status','meta','date','time');
+$UNDERQL['table']['underql'] = array('title','description');
+
+
+/*
+
+underQL uses filters to do something with data before insert or update theme
+ like clean XSS, SQL injection or trim the value. However, you can write your
+ own filter by writing the function with the following pattern :
+
+   function uql_filter_[filtername]($value){
+
+     return $value_after_apply_filter;
+   }
+
+  After that you can apply the filter like the following :
+
+   $_->filter('filtername','name','title',...);
+
+  The parameters that are coming after the filter name are the fields names
+   that you intent to apply your filter on their values before insert or update
+    them.
+
+*/
+// <!-- Filter APIs Begin -->
+
+$UNDERQL['filter']['prefix'] = 'uql_filter_';
+
+
+function uql_filter_xss($value)
+{
+   return strip_tags($value);
+}
+
+function uql_filter_trim($value)
+{
+   return trim($value);
+}
+
+function uql_filter_sqli($value)
+{
+   return mysql_escape_string($value);
+}
+
+
+// <!-- Filter APIs END   -->
+// <!-- Checker APIs Begin -->
+$UNDERQL['checker']['prefix'] = 'uql_checker_';
+
+function uql_checker_email($value)
+{
+
+   return filter_var($value,FILTER_VALIDATE_EMAIL);
+}
+
+
+// <!-- Checker APIs END   -->
+
+
+class underQL{
+
+//used by insert instruction
+private $data_buffer; // used to stor key/value that entered by user
+private $string_fields; // contains the name of all string fields to use it to add single qoute to the value.
+private $table_name; // table name that is accepting all instructions from the object
+
+
+// DB connectivity
+
+private $db_handle;
+private $db_query_result;
+private $db_current_object;
+
+/* Initialization */
+public function __construct()
+{
+   global $UNDERQL;
+
+   $this->db_handle = @mysql_connect($UNDERQL['db']['host'],
+                                     $UNDERQL['db']['user'],
+                                     $UNDERQL['db']['password']);
+
+   if(!$this->db_handle)
+    die('UnderQL Error : Unable to connect to DB..!');
+
+   if(!(@mysql_select_db($UNDERQL['db']['name'])))
+     {
+       @mysql_close($this->db_handle);
+       die('UnderQL Error: Unable to select DB..!');
+     }
+
+   @mysql_query("SET NAMES '".$UNDERQL['db']['encoding']."'");
+
+   $this->db_current_object = null;
+   $this->db_query_result   = false;
+
+
+   $this->clearDataBuffer();
+
+}
+
+private function clearDataBuffer()
+{
+  $this->data_buffer = array();
+}
+
+public function table($tname)
+{
+  global $UNDERQL;
+  $this->table_name = $tname;
+  $this->string_fields = $UNDERQL['table'][$tname];
+}
+
+public function __invoke($tname,$cols='*',$where=null)
+{
+  $this->table($tname);
+
+  return $this->select($cols,$where);
+}
+
+
+public function __set($key,$val)
+{
+ /* if(in_array($key,$this->string_fields))
+   $this->data_buffer[$key] = "'".$val."'";
+  else   */
+   $this->data_buffer[$key] = $val;
+}
+
+private function qoute()
+{
+  foreach($this->data_buffer as $key=>$val)
+  {
+   if(in_array($key,$this->string_fields))
+   $this->data_buffer[$key] = "'".$val."'";
+  else
+   $this->data_buffer[$key] = $val;
+  }
+}
+
+private function formatInsertCommand()
+{
+  $data_buffer_length = @count($this->data_buffer);
+  if($data_buffer_length == 0)
+   return false;
+
+  $sql = 'INSERT INTO '.$this->table_name.' ';
+
+  $sql_columns = '(';
+  $sql_values  = ' VALUE(';
+  $this->qoute();
+  $i = 0;
+  foreach($this->data_buffer as $k=>$v)
+  {
+    $sql_columns .= $k;
+    $sql_values  .= $v;
+    if(($i + 1) < $data_buffer_length)
+    {
+        $sql_columns .= ',';
+        $sql_values  .= ',';
+    }
+
+     $i++;
+  }
+
+  $sql_columns .=')';
+  $sql_values  .=')';
+
+  $sql .= $sql_columns.$sql_values;
+
+  return $sql;
+
+}
+
+public function insert()
+{
+  $sql_insert_string = $this->formatInsertCommand();
+  if($sql_insert_string == false)
+   return false;
+
+  $l_result = $this->query($sql_insert_string);
+  $this->clearDataBuffer();
+  return $l_result;
+}
+
+private function formatUpdateCommand($where = null)
+{
+  $data_buffer_length = @count($this->data_buffer);
+  if($data_buffer_length == 0)
+   return false;
+
+  $sql = 'UPDATE '.$this->table_name.' SET ';
+
+  $sql_clues = '';
+
+  $this->qoute();
+
+  $i = 0;
+  foreach($this->data_buffer as $k=>$v)
+  {
+     $sql_clues .= ' '.$k.'='.$v;
+    if(($i + 1) < $data_buffer_length)
+     $sql_clues .= ',';
+
+     $i++;
+  }
+
+
+  $sql .= $sql_clues;
+
+  if($where != null)
+   $sql .= ' WHERE '.$where;
+
+  return $sql;
+
+}
+
+
+public function update($where = null)
+{
+  $sql_update_string = $this->formatUpdateCommand($where);
+  if($sql_update_string == false)
+   return false;
+
+  $l_result = $this->query($sql_update_string);
+  $this->clearDataBuffer();
+  return $l_result;
+}
+
+
+private function formatDeleteCommand($where = null)
+{
+
+  $sql = 'DELETE FROM '.$this->table_name;
+
+  if($where != null)
+   $sql .= ' WHERE '.$where;
+
+  return $sql;
+
+}
+
+public function delete($where = null)
+{
+  $sql_delete_string = $this->formatDeleteCommand($where);
+
+  $l_result = $this->query($sql_delete_string);
+  $this->clearDataBuffer();
+  return $l_result;
+}
+
+private function formatSelectCommand($cols = '*',$where = null)
+{
+  $sql = 'SELECT '.$cols.' FROM '.$this->table_name;
+
+  if($where != null)
+   $sql .= ' WHERE '.$where;
+
+  return $sql;
+
+}
+
+private function select($cols = '*',$where=null)
+{
+  $sql_select_string = $this->formatSelectCommand($cols,$where);
+
+   $l_result = $this->query($sql_select_string);
+   $this->clearDataBuffer();
+ //echo $sql_select_string;
+  if(@mysql_num_rows($this->db_query_result) == 0)
+     return false;
+
+     $this->fetch(); // get first record
+   return $l_result;
+}
+
+public function fetch()
+{
+   if($this->db_query_result)
+    $this->db_current_object = @mysql_fetch_object($this->db_query_result);
+}
+
+public function count()
+{
+  if(!$this->db_query_result)
+   return 0;
+
+  return @mysql_num_rows($this->db_query_result);
+}
+
+public function free()
+{
+  if($this->db_query_result)
+   @mysql_free_result($this->db_query_result);
+}
+
+public function filter()
+{
+  global $UNDERQL;
+  $l_args_num = func_num_args();
+  if($l_args_num < 2)
+   return false;
+
+  $l_filter_callback = $UNDERQL['filter']['prefix'].func_get_arg(0);
+  if(!function_exists($l_filter_callback))
+   return false;
+
+  for($i = 1; $i < $l_args_num; $i++)
+     $this->data_buffer[func_get_arg($i)] = $l_filter_callback($this->data_buffer[func_get_arg($i)]);
+
+  return true;
+}
+
+public function checker($checker,$value){
+
+   global $UNDERQL;
+   $checker_callback = $UNDERQL['checker']['prefix'].$checker;
+   if(!function_exists($checker_callback))
+    return false;
+
+   return $checker_callback($this->data_buffer[$value]);
+
+}
+
+/*public function apply(){
+
+}*/
+
+public function query($query)
+{
+    $this->free();
+    $this->db_query_result = @mysql_query($query);
+    if($this->db_query_result)
+      return true;
+
+   return false;
+}
+
+public function __get($key)
+{
+   if($this->db_current_object)
+    return $this->db_current_object->$key;
+
+   return '';
+}
+
+}
+
+ /* underQL instance (object) */
+$_ = new underQL();
+
+?>
