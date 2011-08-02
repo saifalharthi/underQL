fc84e9da0fb53a031578057428b4d42507a150e6
Abdullah Almehmadi <cs.almehmadi@gmail.com>
2011-08-02 15:56:38 +0300

    Modify langs/english.php by adding more elements.

--- src/multi/langs/english.php
+++ src/multi/langs/english.php
@@ -9,7 +9,11 @@ $UNDERQL['rule']['uql_fail_messages'] = array(
     'between'  => 'The value of %s must be between %d and less than or equal to %d',
     'hex'      => 'The value of %s must be a Hexdecimal characters [0-9] or [A-F]',
     'alpha'    => 'The value of %s must be alphabet',
-    'alphanum' => 'The value of %s must be mixed of alphabet and numbers'
+    'alphanum' => 'The value of %s must be mixed of alphabet and numbers',
+    'gthan'    => 'The value of %s must be greater than %d',
+    'lthan'    => 'The value of %s must be less than %d',
+    'email'    => 'The value of %s must be a valid e-mail syntax',
+    'ip'       => 'The value of %s must be a valid IP syntax'
 
 );
 
