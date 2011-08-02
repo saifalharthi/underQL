f2272e4b51e398ebf1f829efd10df0cb1cb7c076
Abdullah Almehmadi <cs.almehmadi@gmail.com>
2011-08-02 15:51:21 +0300

    Modify langs/english.php file.

--- src/multi/langs/english.php
+++ src/multi/langs/english.php
@@ -5,7 +5,11 @@ $UNDERQL['rule']['uql_fail_messages'] = array(
     'length'   => 'Length of %s was exceeded the maximum length (%d)',
     'required' => '%s value is required',
     'number'   => 'You must put numerical value in %s',
-    'symbols'  => 'You must put symbols only as a value for %s'
+    'symbols'  => 'You must put symbols only as a value for %s',
+    'between'  => 'The value of %s must be between %d and less than or equal to %d',
+    'hex'      => 'The value of %s must be a Hexdecimal characters [0-9] or [A-F]',
+    'alpha'    => 'The value of %s must be alphabet',
+    'alphanum' => 'The value of %s must be mixed of alphabet and numbers'
 
 );
 
